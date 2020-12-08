868bacf0a34ebbc0d6aa87058dec29b8cc392615
Dominique Hazael-Massieux <dom@w3.org>
2020-12-08 22:13:14 +0100

    Apply live-patch to idlharness to cater for proxied interfaces

--- puppeteer.js
+++ puppeteer.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 const fetch = require("node-fetch");
+const atob = require('atob');
+const btoa = require('btoa');
 
 let browser;
 
@@ -10,6 +12,66 @@ async function getNewPage(debug) {
   }
   return await browser.newPage();
 }
+const requestCache = new Map();
+
+function transform(source) {
+  // insert the magic ___orig owner for interface objects for idlharness
+  // Brittle - this depends on stability of
+  // https://github.com/web-platform-tests/wpt/blob/1357df60a2ab611f4389d1837a7f9fc10548ebc0/resources/idlharness.js#L1436
+  return source.replace("return this.get_interface_object_owner()[this.name];", `
+  const owner = this.get_interface_object_owner();
+  if (owner.___orig[this.name]) return owner.___orig[this.name];
+  return this.get_interface_object_owner()[this.name];
+`);
+}
+
+// from https://gist.github.com/jsoverson/4fe67f835af8c64189a643b5c527d9dc#file-puppeteer-prettier-js-L42 per https://jsoverson.medium.com/using-chrome-devtools-protocol-with-puppeteer-737a1300bac0
+async function intercept(page, patterns, transform) {
+  const client = await page.target().createCDPSession();
+
+  await client.send('Network.enable');
+
+  await client.send('Network.setRequestInterception', { 
+    patterns: patterns.map(pattern => ({
+      urlPattern: pattern, resourceType: 'Script', interceptionStage: 'HeadersReceived'
+    }))
+  });
+
+  client.on('Network.requestIntercepted', async ({ interceptionId, request, responseHeaders, resourceType }) => {
+
+    const response = await client.send('Network.getResponseBodyForInterception',{ interceptionId });
+
+    const contentTypeHeader = Object.keys(responseHeaders).find(k => k.toLowerCase() === 'content-type');
+    let newBody, contentType = responseHeaders[contentTypeHeader];
+
+    if (requestCache.has(response.body)) {
+      newBody = requestCache.get(response.body);
+    } else {
+      const bodyData = response.base64Encoded ? atob(response.body) : response.body;
+      try {
+        if (resourceType === 'Script' && request.url.match(/\/resources\/idlharness.js$/)) newBody = transform(bodyData);
+        else newBody = bodyData
+      } catch(e) {
+        newBody = bodyData
+      }
+  
+      requestCache.set(response.body, newBody);
+    }
+
+    const newHeaders = [
+      'Date: ' + (new Date()).toUTCString(),
+      'Connection: closed',
+      'Content-Length: ' + newBody.length,
+      'Content-Type: ' + contentType
+    ];
+
+    client.send('Network.continueInterceptedRequest', {
+      interceptionId,
+      rawResponse: btoa('HTTP/1.1 200 OK' + '\r\n' + newHeaders.join('\r\n') + '\r\n\r\n' + newBody)
+    });
+  });
+}
+
 
 async function runOnLinkedPages(url, shortname, debug =false) {
   const page = await getNewPage(debug);
@@ -42,7 +104,11 @@ async function runWithProxy(url, shortname, debug = false, noclose = false) {
   const {idlparsed} = await fetch(`https://w3c.github.io/webref/ed/idlparsed/${shortname}.json`).then(r => r.json());
 
   const page = await getNewPage(debug);
-
+  intercept(page, ['*'], transform);
+  browser.on('targetcreated', async (target) => {
+    const page = await target.page();
+    intercept(page, ['*'], transform);
+  });
   page.on('console', msg => {
     console.error(msg.args().map(o => JSON.stringify(o._remoteObject.value, null, 2)).join('\n'));
   });
